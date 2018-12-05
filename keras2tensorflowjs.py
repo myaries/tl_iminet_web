660f8f8b6eb06f01a14d534f79cbbfb576ba253e
myaries <myaries7@gmail.com>
2018-12-05 02:03:42 -0500

    Delete keras2tensorflowjs.py

--- keras2tensorflowjs.py
+++
@@ -1,7 +0,0 @@
-import keras
-from keras.models import load_model
-import tensorflowjs as tfjs
-
-overall_model = load_model('./tl_iminet_keras_version.h5')
-tfjs.converters.save_keras_model(overall_model, './tl-iminet/')
-print('Done!')
